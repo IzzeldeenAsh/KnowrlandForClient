a12341bd8ed2fd3ea00a12cde244177aa2a94d05
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-02-04 11:15:18 +0300

    push

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroLight from '@/components/hero-light'
+import FooterLight from '@/components/ui/footer-light'
+import HeaderLight from '@/components/ui/header-light'
+export default function HomePage() {
+  return (
+   <main className=''>
+    <HeaderLight />
+    <HeroLight />
+    <FooterLight/>
+    </main>
+  )
+}
