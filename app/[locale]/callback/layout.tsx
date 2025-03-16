c2c267fa7c66e530989627e266f4e518c9993d8c
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-03-16 14:01:16 +0300

    commir

--- app/[locale]/callback/layout.tsx
+++ app/[locale]/callback/layout.tsx
@@ -2,10 +2,8 @@ import { Locale } from '@/i18n/routing';
 
 export default function CallbackLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { locale: Locale };
 }) {
   return (
     <div className="callback-container">
