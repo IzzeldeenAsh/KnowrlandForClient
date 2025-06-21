7cfa00cf4d75930c0cc0873c7ba396c0ba7e62c4
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-06-21 17:11:44 +0300

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px', // Custom breakpoint for very large screens
+    },
     extend: {
       fontFamily: {
         inter: ['var(--font-inter)', 'sans-serif'],
