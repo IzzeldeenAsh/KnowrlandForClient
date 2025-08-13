2d65f984bae70b620c12616796d3bab34aaffc40
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-08-13 19:22:56 +0300

    update

--- components/auth/GlobalAuthHandler.tsx
+++ components/auth/GlobalAuthHandler.tsx
@@ -55,13 +55,6 @@ export default function GlobalAuthHandler() {
         return;
       }
 
-      console.log('[GlobalAuthHandler] Auth check:', {
-        hasCookieToken: !!cookieToken,
-        hasLocalStorageToken: !!localStorageToken,
-        hasUserData: !!userData,
-        domain: window.location.hostname,
-        pathname
-      });
 
       // If we have localStorage data but no cookie token, we need to clean up and redirect
       if (!cookieToken && (localStorageToken || userData)) {
