e9dfa2d59aab22842893e3cebb73bf0e4bdff988
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-08-18 19:02:04 +0300

    push

--- app/[locale]/knowledge/[type]/[slug]/page.tsx
+++ app/[locale]/knowledge/[type]/[slug]/page.tsx
@@ -252,7 +252,7 @@ export default function KnowledgePage({ params }: Props) {
               </div>
               <div className="flex flex-col items-start">
                 <div className="flex flex-col items-start mb-6 sm:mb-10">
-                  <h3 className="text-md sm:text-2xl md:text-4xl bg-gradient-to-r from-blue-500 to-teal-400 font-extrabold text-transparent bg-clip-text ">
+                  <h3 className="text-3xl  md:text-4xl bg-gradient-to-r from-blue-500 to-teal-400 font-extrabold text-transparent bg-clip-text ">
                     {knowledge.title}
                   </h3>
                   
