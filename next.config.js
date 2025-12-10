8770aeecb631c40fa95c208afe2b311492aaa494
Izzeldeen Ashour <ezraashour00@gmail.com>
2025-12-10 21:29:08 +0300

    update

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: https: blob:",
               "font-src 'self' data: https://fonts.gstatic.com",
-              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com",
+              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://api.insightabusiness.com",
               "frame-ancestors 'none'",
               "base-uri 'self'",
               "form-action 'self'",
